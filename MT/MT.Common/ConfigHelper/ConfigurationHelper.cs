fce543112f640627730b6ec246d14bee34a2e0af
罗建军 <493409332@qq.com>
2017-06-05 17:59:58 +0800

    泛型配置

--- MT/MT.Common/ConfigHelper/ConfigurationHelper.cs
+++ MT/MT.Common/ConfigHelper/ConfigurationHelper.cs
@@ -6,11 +6,13 @@ using Microsoft.Extensions.Configuration.Binder;
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Text;
-
+using System.Reflection;
+using System.Linq;
 namespace MT.Common.ConfigHelper
 {
- 
+    /// <summary>
+    /// 配置类型
+    /// </summary>
     public enum ConfigurationType
     {
         XML,
@@ -22,31 +24,66 @@ namespace MT.Common.ConfigHelper
     {
         static ConfigurationBuilder configbuilder = new ConfigurationBuilder();
 
-      
+
+
         /// <summary>
         /// 根据节点获取配置
         /// </summary>
+        /// <typeparam name="T"> </typeparam>
         /// <param name="FileName">文件名</param>
         /// <param name="key">节点 key ; key:value</param>
+        /// <param name="configtype">配置类型</param>
         /// <param name="BasePath">文件目录</param>
-        /// <returns></returns>
+        /// <returns>返回对应泛型值</returns>
         public static T GetConfigurationValue<T>(string FileName, string key, ConfigurationType configtype = ConfigurationType.JSON, string BasePath = "") 
         {
             string BasePat = BasePath.Length == 0 ? Directory.GetCurrentDirectory() : BasePath;
-            switch (configtype)
+           Type type= typeof(T);
+            List<Type> typelist = new List<Type>();
+            typelist.Add(typeof(bool));
+            typelist.Add(typeof(byte));
+            typelist.Add(typeof(char));
+            typelist.Add(typeof(decimal));
+            typelist.Add(typeof(double));
+            typelist.Add(typeof(float));
+            typelist.Add(typeof(int));
+            typelist.Add(typeof(long));
+            typelist.Add(typeof(sbyte));
+            typelist.Add(typeof(short));
+            typelist.Add(typeof(uint));
+            typelist.Add(typeof(ulong));
+            typelist.Add(typeof(ushort));
+            typelist.Add(typeof(string));
+            typelist.Add(typeof(object)); 
+            if (typelist.Contains(type))
             {
-                case ConfigurationType.XML:
-                    return new ConfigurationBuilder().SetBasePath(BasePat).AddInMemoryCollection().AddXmlFile(FileName).Build().GetValue<T>(key);
-                case ConfigurationType.JSON:
-                    return new ConfigurationBuilder().SetBasePath(BasePat).AddJsonFile(FileName).Build().GetValue<T>(key);
-                case ConfigurationType.INI:
-                    return new ConfigurationBuilder().SetBasePath(BasePat).AddIniFile(FileName).Build().GetValue<T>(key);
-                case ConfigurationType.Memory:
-                    return configbuilder.AddInMemoryCollection().Build().GetValue<T>(key);
-                default:
-                    return default(T);
+                switch (configtype)
+                {
+                    case ConfigurationType.XML:
+                        return new ConfigurationBuilder().SetBasePath(BasePat).AddInMemoryCollection().AddXmlFile(FileName).Build().GetValue<T>(key);
+                    case ConfigurationType.JSON:
+                        return new ConfigurationBuilder().SetBasePath(BasePat).AddJsonFile(FileName).Build().GetValue<T>(key);
+                    case ConfigurationType.INI:
+                        return new ConfigurationBuilder().SetBasePath(BasePat).AddIniFile(FileName).Build().GetValue<T>(key);
+                    case ConfigurationType.Memory:
+                        return configbuilder.AddInMemoryCollection().Build().GetValue<T>(key);
+                    default:
+                        return default(T);
+                }
             }
- 
+            else
+            {
+                T model = Activator.CreateInstance<T>();
+                foreach (var item in type.GetProperties())
+                {
+                    item.SetValue(model, typeof(ConfigurationHelper).GetMethod("GetConfigurationValue").MakeGenericMethod(item.PropertyType).Invoke(null, new object[] { FileName, string.Format("{0}:{1}", key, item.Name), configtype,"" }));
+                }
+                return model;
+
+            }
+
+
+
         }
 
         /// <summary>
@@ -79,8 +116,7 @@ namespace MT.Common.ConfigHelper
             var defaultSettings = new MemoryConfigurationSource();
             defaultSettings.InitialData = Data;
             configbuilder.Add(defaultSettings);
-
-         
+             
         }
 
     }
--- MT/MT.UnitTest/UnitTest1.cs
+++ MT/MT.UnitTest/UnitTest1.cs
@@ -1,15 +1,98 @@
-using System;
+﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Microsoft.Extensions.Configuration;
+using MT.Common.ConfigHelper;
+using System.Collections.Generic;
+
 namespace MT.UnitTest
 {
+
+    public class TestModel
+    {
+        public TestModel() { }
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+
+    }
+
     [TestClass]
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestGetConfigurationValue()
         {
-                
+            string data = ConfigurationHelper.GetConfigurationValue<string>("test.json", "data", ConfigurationType.JSON);
+            Assert.AreEqual(data, "test");  
+            int TestModel_ID = ConfigurationHelper.GetConfigurationValue<int>("test.json", "TestModel:ID", ConfigurationType.JSON); 
+            string TestModel_Name = ConfigurationHelper.GetConfigurationValue<string>("test.json", "TestModel:Name", ConfigurationType.JSON);
+            TestModel TestModel = ConfigurationHelper.GetConfigurationValue<TestModel>("test.json", "TestModel", ConfigurationType.JSON); 
+            Assert.AreEqual(TestModel_ID, 1);
+            Assert.AreEqual(TestModel_Name, "张三");
+
+            Assert.AreEqual(TestModel.ID, 1);
+            Assert.AreEqual(TestModel.Name, "张三");
+
+            data = ConfigurationHelper.GetConfigurationValue<string>("test.xml", "data", ConfigurationType.XML);
+            Assert.AreEqual(data, "test"); 
+            TestModel_ID = ConfigurationHelper.GetConfigurationValue<int>("test.xml", "TestModel:ID", ConfigurationType.XML); 
+            TestModel_Name = ConfigurationHelper.GetConfigurationValue<string>("test.xml", "TestModel:Name", ConfigurationType.XML); 
+        //    TestModel = ConfigurationHelper.GetConfigurationValue<TestModel>("test.xml", "TestModel", ConfigurationType.XML);
+            Assert.AreEqual(TestModel_ID, 1);
+            Assert.AreEqual(TestModel_Name, "张三");
+            // Assert.AreEqual(TestModel, new TestModel { ID = 1, Name = "张三" });
+
+
+            data = ConfigurationHelper.GetConfigurationValue<string>("test.ini", "data", ConfigurationType.INI);
+            Assert.AreEqual(data, "test");
+            TestModel_ID = ConfigurationHelper.GetConfigurationValue<int>("test.ini", "TestModel:ID", ConfigurationType.INI);
+            TestModel_Name = ConfigurationHelper.GetConfigurationValue<string>("test.ini", "TestModel:Name", ConfigurationType.INI);
+       //     TestModel = ConfigurationHelper.GetConfigurationValue<TestModel>("test.ini", "TestModel", ConfigurationType.INI);
+            Assert.AreEqual(TestModel_ID, 1);
+            Assert.AreEqual(TestModel_Name, "张三");
+            // Assert.AreEqual(TestModel, new TestModel { ID = 1, Name = "张三" });
+
+            List<KeyValuePair<string, string>> Data = new List<KeyValuePair<string, string>>();
+            Data.Add(new KeyValuePair<string, string>("TestModel:ID","1"));
+            Data.Add(new KeyValuePair<string, string>("TestModel:Name", "张三"));
+            ConfigurationHelper.SetMemoryConfiguration(Data);
+        //    TestModel = ConfigurationHelper.GetConfigurationValue<TestModel>(null, "TestModel", ConfigurationType.Memory);
+            TestModel_ID = ConfigurationHelper.GetConfigurationValue<int>(null, "TestModel:ID", ConfigurationType.Memory);
+            TestModel_Name = ConfigurationHelper.GetConfigurationValue<string>(null, "TestModel:Name", ConfigurationType.Memory);
+
+            Assert.AreEqual(TestModel_ID, 1);
+            Assert.AreEqual(TestModel_Name, "张三");
+            // Assert.AreEqual(TestModel, new TestModel { ID = 1, Name = "张三" });
+
+        }
+
+        [TestMethod]
+        public void TestGetConfiguration()
+        {
+            TestModel TestModel = ConfigurationHelper.GetConfiguration<TestModel>("testModel.json", ConfigurationType.JSON); 
+            Assert.AreEqual(TestModel.ID, 1);
+            Assert.AreEqual(TestModel.Name, "张三");
+
+            TestModel = ConfigurationHelper.GetConfiguration<TestModel>("testModel.xml", ConfigurationType.XML);
+            Assert.AreEqual(TestModel.ID, 1);
+            Assert.AreEqual(TestModel.Name, "张三");
+
+
+            TestModel = ConfigurationHelper.GetConfiguration<TestModel>("testModel.ini", ConfigurationType.INI);
+            Assert.AreEqual(TestModel.ID, 1);
+            Assert.AreEqual(TestModel.Name, "张三");
+
+            List<KeyValuePair<string, string>> Data = new List<KeyValuePair<string, string>>();
+            Data.Add(new KeyValuePair<string, string>("ID", "1"));
+            Data.Add(new KeyValuePair<string, string>("Name", "张三"));
+            ConfigurationHelper.SetMemoryConfiguration(Data);
+
+            TestModel = ConfigurationHelper.GetConfiguration<TestModel>(null, ConfigurationType.Memory);
+            Assert.AreEqual(TestModel.ID, 1);
+            Assert.AreEqual(TestModel.Name, "张三");
+
+
         }
     }
 }
